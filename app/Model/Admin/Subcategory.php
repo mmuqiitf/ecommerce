7fc3d0d13bc32a76039520652caf642ecef3df6a
M Muqiit Faturrahman <mmuqiit.f14@gmail.com>
2020-07-27 12:25:21 +0700

    first commit from local

---
+++ app/Model/Admin/Subcategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subcategory extends Model
+{
+    protected $table = 'subcategories';
+    protected $guarded = [];
+}
